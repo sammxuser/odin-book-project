29b2aa4e21e3156c842298eb0462eb27a63b08d9
Samuel Mburu <sammxuser@gmail.com>
2024-05-29 23:23:10 +0300

    Add book capability added

--- script.js
+++ script.js
@@ -24,24 +24,41 @@ function Book(title, author, pages, isRead) {
   };
 }
 
-function addBookToLibrary(Book) {}
+function addBookToLibrary() {
+  const bookTitle = prompt('Book Title');
+  const bookAuthor = prompt(bookTitle + ' author :');
+  const bookPages = prompt('No. of pages ');
+  const isBookRead = prompt('Have you read the book?(Y/N)');
+  const bookReadStatus = isBookRead === 'Y' ? true : false;
+  const newBook = new Book(bookTitle, bookAuthor, bookPages, bookReadStatus);
+  myLibrary.push(newBook);
+}
 
 // A function that loops through the library and displays
-function displayBook() {}
+function displayBook() {
+  const bookList = document.querySelector('.book-shelf');
 
-const bookList = document.querySelector('.book-shelf');
+  myLibrary.forEach((element) => {
+    const bookItem = document.createElement('div');
+    bookItem.classList = 'card';
+    const cardBody = document.createElement('div');
+    cardBody.classList = 'container';
+    const cardHeader = document.createElement('h4');
+    cardHeader.textContent = element.title;
+    cardBody.appendChild(cardHeader);
+    const cardContent = document.createElement('p');
+    cardContent.textContent = element.author + ', Pages ' + element.pages;
+    cardBody.appendChild(cardContent);
+    bookItem.appendChild(cardBody);
+    bookList.appendChild(bookItem);
+  });
+}
 
-myLibrary.forEach((element) => {
-  const bookItem = document.createElement('div');
-  bookItem.classList = 'card';
-  const cardBody = document.createElement('div');
-  cardBody.classList = 'container';
-  const cardHeader = document.createElement('h4');
-  cardHeader.textContent = element.title;
-  cardBody.appendChild(cardHeader);
-  const cardContent = document.createElement('p');
-  cardContent.textContent = element.author + ', Pages ' + element.pages;
-  cardBody.appendChild(cardContent);
-  bookItem.appendChild(cardBody);
-  bookList.appendChild(bookItem);
+const addABookLink = document.getElementById('add-a-book-link');
+addABookLink.addEventListener('click', () => {
+  addBookToLibrary();
+  const bookList = document.querySelector('.book-shelf');
+  bookList.textContent = '';
+  displayBook();
 });
+displayBook();
